d56d393439bd43ed9943151ff640bb82e8f43421
Andrew <anisimov.andrew.hi@gmail.com>
2021-03-14 11:40:45 +0300

    Added post method for creating entity

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,12 +1,14 @@
 package org.example.models;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class Person {
     private int id;
     private String name;
