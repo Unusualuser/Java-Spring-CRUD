55ef392c88c3c79c25c9748bb55715ef9b94f1d8
Andrew <anisimov.andrew.hi@gmail.com>
2021-03-14 14:10:09 +0300

    Validation of forms and fields using annotations

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -1,5 +1,6 @@
 package org.example.models;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,8 +12,17 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+
+    @NotEmpty(message = "Age should not be empty")
+    @Min(value = 0, message = "Age should not be less than 0")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
 }
